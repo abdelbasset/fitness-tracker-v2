d9cbf5f16be810a3a2f68f0c501af83f117f7ab0
AB <abagga@elyosdigital.com>
2019-09-30 11:50:03 +0100

    Routing & Authentication

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -7,9 +9,25 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class SidenavListComponent implements OnInit {
   @Output() closeSidenav = new EventEmitter<void>();
-  constructor() { }
+  
+  isAuth = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  onLogout(){
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
   }
 
   onClose(){
