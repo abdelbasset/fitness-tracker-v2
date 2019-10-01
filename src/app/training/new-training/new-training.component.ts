bcdc1a346df5c4d603584e01ac3be0fdf9521441
AB <abagga@elyosdigital.com>
2019-10-01 16:20:41 +0100

    Setting & Controlling an Active Exercise and Adding a Form to the Training Component

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
@@ -8,17 +9,14 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  @Output() traningStart = new EventEmitter<void>();
   constructor(private trainingService: TrainingService) { }
   exercises : Exercise [] = [];
 
   ngOnInit() {
     this.exercises = this.trainingService.getAvilableExercises();
-
-    console.log(this.exercises);
   }
-  onStartTraining(){
-    this.traningStart.emit();
+  onStartTraining(form: NgForm){
+    this.trainingService.startExercise(form.value.exercise)
   }
 
 }
