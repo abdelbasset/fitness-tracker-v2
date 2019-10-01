29de630a30e2283875f320ee1ca0972426da6957
AB <abagga@elyosdigital.com>
2019-10-01 16:54:44 +0100

    Handling Complete and Cancel Events

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -11,18 +11,40 @@ export class TrainingService {
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
     ];
 
-    private runningExercise : Exercise;
-
-    getAvilableExercises(){
+    private runningExercise: Exercise;
+    private exercises: Exercise[] = [];
+    getAvilableExercises() {
         return this.availableExercises.slice();
     }
 
-    startExercise(selectedId: string){
+    startExercise(selectedId: string) {
         this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
         this.exerciseChanged.next({ ...this.runningExercise });
     }
 
-    getRunningExercise(){
+    completeExercise() {
+        this.exercises.push({ 
+            ...this.runningExercise, 
+            date: new Date(), 
+            state: 'completed' 
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    cancelExercise(progress: number){
+        this.exercises.push({ 
+            ...this.runningExercise, 
+            duration: this.runningExercise.duration * (progress / 100),
+            calories: this.runningExercise.duration * (progress / 100),
+            date: new Date(), 
+            state: 'cancelled' 
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    getRunningExercise() {
         return { ...this.runningExercise };
     }
 }
