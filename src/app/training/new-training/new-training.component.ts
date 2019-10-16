1ea0cfef23e70115b3616861734ffd2f631691c0
AB <abagga@elyosdigital.com>
2019-10-16 18:32:57 +0100

    use pipe & map operators

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,6 +4,7 @@ import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-new-training',
@@ -13,12 +14,28 @@ import { Observable } from 'rxjs';
 export class NewTrainingComponent implements OnInit {
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
   //exercises: Exercise[] = [];
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
 
   ngOnInit() {
     //this.exercises = this.trainingService.getAvilableExercises();
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.db.collection('availableExercises')
+           .snapshotChanges()
+           .pipe(map(docArray => { 
+            console.log(docArray);
+              return docArray.map(doc => { 
+                return {
+                  id: '1',
+                  name: 'test',
+                  duration: 10,
+                  calories: 20
+
+                };
+              });
+            }))
+           .subscribe(result => {
+            console.log(result);
+    })
   }
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise)
