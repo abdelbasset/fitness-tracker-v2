df49eac36d08d480785490782017ffa2e9b74c27
AB <abagga@elyosdigital.com>
2019-11-06 14:33:40 +0100

    Add collection finishedExercises to Firebase

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -43,7 +43,7 @@ export class TrainingService {
     }
 
     completeExercise() {
-        this.exercises.push({ 
+        this.addDataToDatabase({ 
             ...this.runningExercise, 
             date: new Date(), 
             state: 'completed' 
@@ -53,7 +53,7 @@ export class TrainingService {
     }
 
     cancelExercise(progress: number){
-        this.exercises.push({ 
+        this.addDataToDatabase({ 
             ...this.runningExercise, 
             duration: this.runningExercise.duration * (progress / 100),
             calories: this.runningExercise.calories * (progress / 100),
@@ -70,5 +70,9 @@ export class TrainingService {
 
     getCompletedOrCancelledExercises(){
         return this.exercises.slice();
+    }
+
+    private addDataToDatabase(exercise: Exercise){
+        this.db.collection('finishedExercises').add(exercise);
     }
 }
