a3743ccd37e70ae7edfbfb20f8f5a97ac2200762
AB <abagga@elyosdigital.com>
2019-12-11 16:15:05 +0100

    Cleaning authModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { MaterialModule } from '../material.module';
 import { AngularFireAuthModule } from '../../../node_modules/@angular/fire/auth';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
   imports: [
-    CommonModule,
-    FormsModule,
     ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-  ],
-  exports: []
+    SharedModule
+  ]
 })
 export class AuthModule {}
